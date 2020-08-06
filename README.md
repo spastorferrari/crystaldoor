95c2a71f65e32f4fa4d58a373cec1537220d9a40
sebasrpf <srolimpia@gmail.com>
2020-08-05 23:07:38 -0600

    argparse implemented. final readme.md

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Script that allows you to input a Glassdoor link and extract every job applicati
 * BeautifulSoup
 * Firefox Webdriver
 
-#### How to use:
-* Step 01: Paste your working directory into "YOUR_WORKING_DIRECTORY_HERE". Your wd should be the folder with your script & webdriver.
-* Step 02: Paste the link of the specific page that you want to scrape into "GLASSDOOR_LINK_HERE".
-* Step 03: GO!
+#### How to use ():
+* Step 01: open your terminal and go to crystaldoor folder.
+* Step 02: run python main.py PASTE_WEBSITE_LINK.
+* Step 03: wait a few seconds while the Firefox driver does the scraping...
+* Terminal will return the scrape :).
 
 ![screencap](images/image01.png)
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+# ---------------------------------------------------------- IMPORT DEPENDENCIES
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
-wd = "WORKING_DIRECTORY_HERE"
-page = "GLASSDOOR_LINK_HERE"
-
+import argparse as np
+# --------------------------------------------------------------------- ARGPARSE
+parser = np.ArgumentParser()
+parser.add_argument("link", type=str, help='insert link to parse.')
+args = parser.parse_args()
+page = args.link
+# ------------------------------------------------------------------------ LOGIC
 def glassdoor_questions():
-    import os
-    os.chdir(wd)
-
     glassdoor = webdriver.Firefox()
     glassdoor.get(page)
     html = glassdoor.page_source
-    gdsoup = BeautifulSoup(html)
+    gdsoup = BeautifulSoup(html, features="html.parser")
     descr = list()
     for tag in gdsoup.find_all('p', {"class":"interviewDetails continueReading interviewContent mb-xsm"}):
         descr.append(tag)
         print(tag.text, "\n")
-
 glassdoor_questions()
