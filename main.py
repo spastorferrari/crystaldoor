9acb50f34adb349f0e83b50b263e7c4fcf74fa55
sebasrpf <srolimpia@gmail.com>
2020-08-05 21:21:57 -0600

    final changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import openpyxl
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import os
+os.chdir("C:\\Users\\Sebastian Pasotr\\Documents\\Data_Coding\\crystaldoor")
 
 glassdoor = webdriver.Firefox()
 glassdoor.get("https://www.glassdoor.com/Interview/Google-Product-Manager-Interview-Questions-EI_IE9079.0,6_KO7,22_IP2.htm")
