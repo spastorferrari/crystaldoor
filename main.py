3836f9c470807cf602e7ab082bdf32ba8b67be54
sebasrpf <srolimpia@gmail.com>
2020-08-05 21:38:26 -0600

    ,,,

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import os
 os.chdir("C:\\Users\\Sebastian Pasotr\\Documents\\Data_Coding\\crystaldoor")
 
-glassdoor = webdriver.Firefox()
-glassdoor.get("https://www.glassdoor.com/Interview/Google-Product-Manager-Interview-Questions-EI_IE9079.0,6_KO7,22_IP2.htm")
-html = glassdoor.page_source
-gdsoup = BeautifulSoup(html)
-descr = list()
-for tag in gdsoup.find_all('p', {"class":"interviewDetails continueReading interviewContent mb-xsm"}):
-    descr.append(tag)
-    print(tag.text, "\n")
+def glassdoor_questions(link):
+    glassdoor = webdriver.Firefox()
+    glassdoor.get(link)
+    html = glassdoor.page_source
+    gdsoup = BeautifulSoup(html)
+    descr = list()
+    for tag in gdsoup.find_all('p', {"class":"interviewDetails continueReading interviewContent mb-xsm"}):
+        descr.append(tag)
+        print(tag.text, "\n")
+glassdoor_questions("https://www.glassdoor.com/Interview/Google-Product-Manager-Interview-Questions-EI_IE9079.0,6_KO7,22_IP2.htm")
